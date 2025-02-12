8cf9bf27ce366611efa62a4a83b1c513137a385f
Quantum-cosmos <dy19740207@gmail.com>
2025-02-12 18:00:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def main():
     </style>
     """, unsafe_allow_html=True)
 
-    st.markdown('<div class="main-title"><h1>📚 Interactive Financial Statement Analysis System</h1></div>', 
+    st.markdown('<div class="main-title"><h1>📚 Interactive Electronic Healhth Record Analysis System</h1></div>', 
                 unsafe_allow_html=True)
 
     with st.sidebar:
